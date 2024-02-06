347c75fb2056c0d2e522f34ed4a415727ca897b2
khairul <khairuljf@gmail.com>
2024-02-06 13:14:29 +0600

    add & delete function added on graphql server

--- apps/graphql-server/index.js
+++ apps/graphql-server/index.js
@@ -2,100 +2,100 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import axios from 'axios';
 
-
-
-
-import {typeDefs } from './schema.js'
-
-
+import { typeDefs } from './schema.js';
 
 const resolvers = {
-    Query:{
-
-        contacts: async (parent, {id}) => {
-            const response = await axios.get(`http://localhost:3006/contacts`)
-            const contacts = response.data;
-            return contacts
-        },
-        
-        contact: async (parent, {id}) => {
-            const response = await axios.get(`http://localhost:3006/contacts/${id}`)
-            const ContactData = response.data;
-
-            return {
-                id: ContactData.id,
-                name: ContactData.name,
-                email: ContactData.email,
-               
-            }
-        },
-
-    
-
+  Query: {
+    contacts: async (parent, { id }) => {
+      const response = await axios.get(`http://localhost:3006/contacts`);
+      const contacts = response.data;
+      return contacts;
     },
 
-    Mutation :{
+    contact: async (parent, { id }) => {
+      const response = await axios.get(`http://localhost:3006/contacts/${id}`);
+      const ContactData = response.data;
 
-         addContact: async (_, args)=>{
+      return {
+        id: ContactData.id,
+        name: ContactData.name,
+        email: ContactData.email,
+      };
+    },
+  },
 
-            let contact = {
-                ...args.contact,
-                id:Math.floor(Math.random() * 1000).toString()
-            }
+  Mutation: {
+    addContact: async (_, args) => {
+      let contact = {
+        ...args.contact,
+        id: Math.floor(Math.random() * 1000).toString(),
+      };
 
-            
+      try {
+        const response = await axios.post(
+          'http://localhost:3006/contacts',
+          contact
+        );
 
-            try {
-                const response = await axios.post("http://localhost:3006/contacts", contact);
-
-                if (response.data) {
-                   return contact
-                }
-                } catch (error) {
-                    // Handle errors from the API call
-                    console.error("Error while making the API call:", error);
-                     return error
-                }
-
-
-        },
-
-         updateContact(_, agrs){
-
-            db.games = db.games.map((g)=>{
-                if(g.id===agrs.id){
-                    return {...g, ...agrs.edits}
-                }
-                return g
-            })
-
-            return db.games
-        },
-
-        deleteContact(_, args){
-            return db.games.map((game)=>game.id !== args.id)
+        if (response.data) {
+          return contact;
         }
+      } catch (error) {
+        // Handle errors from the API call
+        console.error('Error while making the API call:', error);
+        return error;
+      }
+    },
 
-       
+    updateContact: async (_, agrs) => {
+      try {
+        const response = await axios.put(
+          `http://localhost:3006/contacts/${agrs?.id}`,
+          agrs
+        );
 
-       
-    }
-}
+        if (response.data) {
+          return response.data;
+        }
+      } catch (error) {
+        // Handle errors from the API call
+        console.error('Error while making the API call:', error);
+        return error;
+      }
+    },
 
+    deleteContact: async (_, args) => {
+      try {
+        const response = await axios.delete(
+          `http://localhost:3006/contacts/${args?.id}`
+        );
+
+        if (response.data) {
+            const response = await axios.get(`http://localhost:3006/contacts`);
+            return  response.data;
+        }
+      } catch (error) {
+        // Handle errors from the API call
+        console.error('Error while making the API call:', error);
+        return error;
+      }
+    },
+  },
+};
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  //  1. creates an Express app
-  //  2. installs your ApolloServer instance as middleware
-  //  3. prepares your app to handle incoming requests
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 8000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 8000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
